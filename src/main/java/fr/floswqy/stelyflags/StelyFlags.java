780db31d80e5d3a91a0b8c306a613bfe4b77811a
Flowsqy <Flo504.Dev@gmail.com>
2021-04-12 22:21:20 +0200

    Add flag registration

---
+++ src/main/java/fr/floswqy/stelyflags/StelyFlags.java
@@ -0,0 +1,32 @@
+package fr.floswqy.stelyflags;
+
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
+
+public class StelyFlags {
+
+    public static StateFlag SPAWN_SPAWNER;
+    public static StateFlag INTERACT_VILLAGER;
+
+    public static void load(FlagRegistry flagRegistry) {
+        if (SPAWN_SPAWNER == null)
+            SPAWN_SPAWNER = registerFlag(flagRegistry, "spawn-spawner");
+        if (INTERACT_VILLAGER == null)
+            INTERACT_VILLAGER = registerFlag(flagRegistry, "interact-villager");
+    }
+
+    private static StateFlag registerFlag(FlagRegistry flagRegistry, String flagName) {
+        final Flag<?> flag = flagRegistry.get(flagName);
+        if (flag == null) {
+            final StateFlag finalFlag = new StateFlag(flagName, false);
+            flagRegistry.register(finalFlag);
+            return finalFlag;
+        }
+        if (flag instanceof StateFlag)
+            return (StateFlag) flag;
+
+        throw new RuntimeException("Can not register the flag '" + flagName + "', another exist with the same name but can not be used");
+    }
+
+}
--- src/main/java/fr/floswqy/stelyflags/StelyFlagsPlugin.java
+++ src/main/java/fr/floswqy/stelyflags/StelyFlagsPlugin.java
@@ -1,7 +1,20 @@
 package fr.floswqy.stelyflags;
 
+import com.sk89q.worldguard.WorldGuard;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public class StelyFlagsPlugin extends JavaPlugin {
 
+    @Override
+    public void onLoad() {
+        try {
+            StelyFlags.load(WorldGuard.getInstance().getFlagRegistry());
+        } catch (RuntimeException exception) {
+            getLogger().log(Level.SEVERE, "Can not load flags, disable the plugin", exception);
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+    }
 }
